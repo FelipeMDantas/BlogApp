4d1cff05e55f1d1059a81a77bd850941daaa4720
FelipeMDantas <felipemdantas@terra.com.br>
2023-09-15 23:19:50 -0300

    Authentication provider and .env variables set.

--- src/components/AuthList/AuthLinks.jsx
+++ src/components/AuthList/AuthLinks.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
 
-  const status = "authenticated";
+  const status = "notAuthenticated";
   return (
     <>
       {status === "notAuthenticated" ? (
