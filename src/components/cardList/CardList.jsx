ca187ccc2de271523ded8510bc5cbd4a4f9ca7b7
FelipeMDantas <felipemdantas@terra.com.br>
2023-09-19 22:43:36 -0300

    Posts fetch and pagination coded.

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -2,7 +2,21 @@ import Card from "../card/Card";
 import Pagination from "../pagination/Pagination";
 import styles from "./cardlist.module.css";
 
-const CardList = () => {
+const getData = async (page) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const CardList = async ({ page }) => {
+  const data = await getData(page);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
