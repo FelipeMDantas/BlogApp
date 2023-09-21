43c90865446d5a08771783b854ec43b91109af6a
FelipeMDantas <felipemdantas@terra.com.br>
2023-09-20 23:03:30 -0300

    Pagination functionality established.

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -2,10 +2,13 @@ import Card from "../card/Card";
 import Pagination from "../pagination/Pagination";
 import styles from "./cardlist.module.css";
 
-const getData = async (page) => {
-  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
-    cache: "no-store",
-  });
+const getData = async (page, cat) => {
+  const res = await fetch(
+    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Failed");
@@ -14,19 +17,21 @@ const getData = async (page) => {
   return res.json();
 };
 
-const CardList = async ({ page }) => {
-  const data = await getData(page);
+const CardList = async ({ page, cat }) => {
+  const { posts, count } = await getData(page, cat);
+  const POST_PER_PAGE = 2;
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {posts?.map((item) => (
+          <Card key={item._id} item={item} />
+        ))}
       </div>
-      <Pagination />
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
   );
 };
